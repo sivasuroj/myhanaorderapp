e0d62706bb8912989f8691e84a2721e10ced5dcc
Sivasuroj s <sivasurojs@gmail.com>
2026-03-26 14:47:57 +0000

    created inputs and asubmit logic added

--- app/custorder/webapp/controller/Orderview.controller.js
+++ app/custorder/webapp/controller/Orderview.controller.js
@@ -1,10 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+],function (Controller,MessageBox) {
     "use strict";
 
     return Controller.extend("custorder.controller.Orderview", {
         onInit() {
+        },
+        submit: function() {
+            var orderID = this.getView().byId("_IDGenInput").getValue();
+            var custID = this.getView().byId("_IDGenInput1").getValue();
+            var empID = this.getView().byId("_IDGenInput2").getValue();
+            var shipID = this.getView().byId("_IDGenInput3").getValue();
+            var oModel = this.getView().getModel();
+
+            var oContext = oModel.bindList("/Orders").create({
+                "OrderID"       : orderID,   
+                "CustomerID"    : custID,
+                "EmployeeID"    : empID,
+                "ShipName"      : shipID
+            });
+            oContext.created().then(()=>{
+                MessageBox.success("Orders added succesfully");
+            }).catch((err)=>{
+                MessageBox.console.error("Orders added succesfully");
+            });              
+            
         }
     });
 });
