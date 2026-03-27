786d04840286a0155b4eb0d7ded97a5c9cc354f0
Sivasuroj s <sivasurojs@gmail.com>
2026-03-27 11:35:54 +0000

    Curd operation implemented

--- app/custorder/webapp/controller/Orderview.controller.js
+++ app/custorder/webapp/controller/Orderview.controller.js
@@ -1,7 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
-],function (Controller,MessageBox) {
+    "sap/m/MessageBox",
+    "sap/ui/core/Fragment",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+],function (Controller,MessageBox,Fragment,MessageToast,Filter,FilterOperator) {
     "use strict";
 
     return Controller.extend("custorder.controller.Orderview", {
@@ -26,6 +30,124 @@ sap.ui.define([
                 MessageBox.console.error("Orders added succesfully");
             });              
             
+        },
+        onCollapseExpandPress() {
+			const oSideNavigation = this.byId("sideNavigation"),
+				bExpanded = oSideNavigation.getExpanded();
+
+			oSideNavigation.setExpanded(!bExpanded);
+		},
+        onAddOrdersPress(){
+            this.hideallPanal();
+            var oPanel= this.byId("addpanel");
+            oPanel.setVisible(true);
+        },
+        onViewOrdersPress(){
+            this.hideallPanal();
+            var oPanel= this.byId("addpanel1");
+            oPanel.setVisible(true);
+        },
+        onViewEditPress(){
+            this.hideallPanal();
+            var oPanel= this.byId("addpanel2");
+            oPanel.setVisible(true);
+        },
+        hideallPanal(){
+            this.byId("addpanel").setVisible(false);
+            this.byId("addpanel1").setVisible(false);
+            this.byId("addpanel2").setVisible(false);
+        },
+        OnActionPressed: function (oevent) {
+            var that=this;
+            var button = oevent.getSource();            
+            var oContent = button.getBindingContext();
+            this._oSelectedContext= oContent;
+
+            if(!this.ActionSheet){
+                Fragment.load({
+                    id:this.getView().getId(),
+                    name:"custorder.view.Actionsheet",
+                    controller:this
+                }).then(function (oActionSheet) {
+                    that.ActionSheet = oActionSheet;
+                    that.getView().addDependent(that.ActionSheet);
+                    that.ActionSheet.openBy(button);
+                }.bind(this));
+            }else{
+                this.ActionSheet.openBy(button);
+            }
+        },
+        OnDeletePress: function (params) {
+            var oContext= this._oSelectedContext;
+            var orders = oContext.getProperty("OrderID");
+            MessageBox.confirm("Do u need to delete the orders:" + orders+"?",{
+                actions : [MessageBox.Action.Yes,MessageBox.Action.NO],
+                onClose: function (oAction) {
+                    if(oAction === MessageBox.Action.Yes){
+                        oContext.delete("$direct").then(function(){
+                            MessageBox.success("deleeted successfully")
+                        }).catch(function (error) {
+                             MessageBox.error("deleeted successfully")
+                        })
+                    }
+                }
+            })
+        },
+        OnEditPress: function () {
+            var that = this
+            var data = this._oSelectedContext.getObject();
+            MessageToast.show("Edit action for item ID: " +data.OrderID);
+            this.onViewEditPress();
+            var omodel= this.getOwnerComponent().getModel();
+            let afilter =[
+                new Filter("OrderID",FilterOperator.EQ,data.OrderID)
+            ];
+            let OBinding = omodel.bindList("/Orders");
+            OBinding.filter(afilter);
+
+            OBinding.requestContexts().then(function (aContexts) {
+                if(aContexts.length > 0){
+                    aContexts.forEach((aContext) => {
+                        let user = aContext.getObject();
+                        that.getView().byId("_IDGenInput17").setValue(user.OrderID);
+                        that.getView().byId("_IDGenInput11").setValue(user.CustomerID);
+                        that.getView().byId("_IDGenInput21").setValue(user.EmployeeID);
+                        that.getView().byId("_IDGenInput31").setValue(user.ShipName);
+                    });
+                }else{
+                    MessageBox.error("Not Found");
+                }
+            }).catch(function (error) {
+                 MessageBox.error("restricted");
+            })
+        },
+        update: function () {
+            var orderID = this.getView().byId("_IDGenInput17").getValue();
+            var custID = this.getView().byId("_IDGenInput11").getValue();
+            var empID = this.getView().byId("_IDGenInput21").getValue();
+            var shipID = this.getView().byId("_IDGenInput31").getValue();
+            var oModel = this.getView().getModel();      
+
+            var spath = "/Orders('" + orderID + "')";
+            var oContext= oModel.bindContext(spath).getBoundContext();
+
+            var oview = this.getView();
+            function resetbusy() {
+                oview.setBusy(false);
+            }
+            oview.setBusy(true);
+            oContext.setProperty("OrderID",orderID);
+            oContext.setProperty("CustomerID",custID);
+            oContext.setProperty("EmployeeID",empID);
+            oContext.setProperty("ShipName",shipID);
+
+            oModel.submitBatch("auto").then(function () {
+                resetbusy();
+                MessageBox.success("Update successfully");
+            }).catch(function (error) {
+                resetbusy();
+                MessageBox.error("Update failed");
+            })
         }
     });
 });
